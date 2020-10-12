79b004ce0cb7d8b25bc6ca374076c518fa48c411
Aaron Bostrom <aaron.bostrom1@gmail.com>
2020-10-12 11:53:06 +0100

    entity

---
+++ examples/entities_example.py
@@ -0,0 +1,11 @@
+
+
+from py_adventure import Entity
+
+
+a : Entity = Entity("entity")
+b : Entity = Entity("entity")
+c : Entity = Entity("entity")
+d : Entity = Entity("entity")
+e : Entity = Entity("entity")
+f : Entity = Entity("entity")
--- py_adventure/entities/entity.py
+++ py_adventure/entities/entity.py
@@ -1,4 +1,28 @@
-
+from __future__ import annotations
+from collections import defaultdict, Counter
+from typing import Counter, Set, ClassVar, Dict
+from weakref import WeakValueDictionary
 
 class Entity:
-    pass
+    # a list of all the current entities.
+    entities : ClassVar[WeakValueDictionary] = WeakValueDictionary()
+    entity_naming : ClassVar[Counter[str]] = Counter()
+
+    def __init__(self, name) -> None:
+        self._name = name 
+        Entity.entity_naming.update([name])
+        count = Entity.entity_naming[name]       
+        if count > 1:
+            self._name+= str(count-1)
+
+        print(self._name)
+        # add the entity to the entities map.
+        Entity.entities[self._name] = self
+
+    def get_name(self) -> str:
+        return self._name
+
+    def __del__(self) -> None:
+        del Entity.entities[self._name]
+
+        print("bye" + self._name)
